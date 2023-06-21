f1cba24e86ab395edc15dc71b8315c256e244d80
hyewon218 <won_11111@naver.com>
2023-06-21 17:31:43 +0900

    RestTemplate Get(getForEntity) 요청

--- src/main/java/com/sparta/springresttemplateserver/service/ItemService.java
+++ src/main/java/com/sparta/springresttemplateserver/service/ItemService.java
@@ -19,12 +19,25 @@ public class ItemService {
             new Item("AirPods", 350_000)
     );
 
+    // 검색에어 맞는지 안 맞는지 체크해서 Item List 에서 찾아서 반환
     public Item getCallObject(String query) {
+        for (Item item : itemList) {
+            // item.getTitle() : 고정되어 있는 데이터 가지고 온다.( Mac, iPad...)
+            // 우리가 받아온 query 랑 비교한다.
+            if(item.getTitle().equals(query)) {
+                // 해당하는 item 객체 하나를 return 한다.
+                return item;
+            }
+        }
         return null;
     }
 
     public ItemResponseDto getCallList() {
-        return null;
+        ItemResponseDto responseDto = new ItemResponseDto();
+        for (Item item : itemList) {
+            responseDto.setItems(item);
+        }
+        return responseDto;
     }
 
     public Item postCall(String query, UserRequestDto requestDto) {
